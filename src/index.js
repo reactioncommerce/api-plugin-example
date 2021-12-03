f1dc7ecfb2349ed443f4c0a9c49226f8378cf3b9
Alvaro Bueno <alvaro.bueno@mailchimp.com>
2021-12-03 18:44:49 -0500

    Revert "feat: enabling typescript"

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import pkg from "../package.json";
  * @param {ReactionAPI} app The ReactionAPI instance
  * @returns {undefined}
  */
-export default async function register(app: any) {
+export default async function register(app) {
   await app.registerPlugin({
     label: "Plugin Example",
     name: "plugin-example",
